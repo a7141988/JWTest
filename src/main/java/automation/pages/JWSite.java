bd90501195391744e41bf3992187f8fb4443a2e9
unknown <axl2807@gmail.com>
2023-01-19 02:08:30 -0500

    updated chromedriver and added email and password validation tests

--- src/main/java/automation/pages/JWSite.java
+++ src/main/java/automation/pages/JWSite.java
@@ -9,20 +9,23 @@ import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 public class JWSite {
     private final WebDriver driver;
 
-    @FindBy(name = "username")
+    //Xpath can be found in google chrome via right click -> Inspect.  Then click on element and right click -> Copy -> Copy XPath
+    //Xpath needs to be surrounded by double quotes
+    @FindBy(xpath = "//*[@id=\"jw-dash-root\"]/div/wui-theme-section/div/div[1]/form/wui-theme-section/div/div/wui-box/div/div[1]/wui-box/div/wui-input/div/input")
     private WebElement emailField;
 
-    @FindBy(name = "password")
+    @FindBy(xpath = "//*[@id=\"jw-dash-root\"]/div/wui-theme-section/div/div[1]/form/wui-theme-section/div/div/wui-box/div/div[2]/wui-box/div/wui-input/div/input")
     private WebElement passwordField;
 
-    @FindBy(css = "button[class='pui-button pui-button--type-primary']")
+    @FindBy(xpath = "//*[@id=\"jw-dash-root\"]/div/wui-theme-section/div/div[1]/form/wui-theme-section/div/div/wui-box/div/wui-button/button/span")
     private WebElement loginButton;
 
-    @FindBy(id = "player-title")
+    @FindBy(xpath = "//*[@id=\"jw-dash-root\"]/div/div[2]/div[2]/wui-theme-section/div/div[1]/div/div/h1")
     private WebElement playerTitle;
 
     @FindBy(css = "button[class='button button-default button-med ng-binding']")
@@ -38,16 +41,23 @@ public class JWSite {
         driver.manage().window().maximize();
     }
 
-    public void load() {
+    public void load() throws Throwable {
         driver.get("https://dashboard.jwplayer.com/");
     }
 
-    public void login(String email, String password) {
+    public void login(String email, String password)  {
         emailField.clear();
         emailField.sendKeys(email);
         passwordField.clear();
         passwordField.sendKeys(password);
         loginButton.click();
+    }
+
+    public void enterEmailClickPasswordField(String email) {
+        emailField.clear();
+        emailField.sendKeys(email);
+        passwordField.clear();
+        passwordField.sendKeys("");
     }
 
     public void clickLink(String linkName) {
@@ -76,5 +86,13 @@ public class JWSite {
     public void checkPlayerNameExists(String playerName) throws Throwable {
         Thread.sleep(1000);
         assertThat(driver.findElement(By.linkText(playerName)).isDisplayed(), is(true));
+    }
+
+    public void verifyWarningText() {
+        assertEquals("The email or password you entered is incorrect. Please try again.", driver.findElement(By.xpath("//*[@id=\"jw-dash-root\"]/div/wui-theme-section/div/div[1]/form/wui-theme-section/div/div/wui-box/div/wui-alert/div")).getText());
+    }
+
+    public void verifyInvalidEmailText() {
+        assertEquals("Please include an '@' in the email address. 'abc' is missing an '@'.", driver.findElement(By.xpath("//*[@id=\"jw-dash-root\"]/div/wui-theme-section/div/div[1]/form/wui-theme-section/div/div/wui-box/div/div[1]/wui-box/div/wui-input/div[2]")).getText());
     }
 }
